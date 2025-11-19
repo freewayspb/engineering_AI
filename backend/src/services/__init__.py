64f844123d0093a9c01dd10ec0a6efda8d4db06f
alexander-sysuiev <alexander.sysuiev@gmail.com>
2025-11-19 17:30:41 +0100

    Cleanup

--- backend/src/services/__init__.py
+++ backend/src/services/__init__.py
@@ -1,5 +1,4 @@
 from .console_json_ollama import run_console_json_ollama
-from .console_vision_ollama import run_console_vision_ollama
 from .json_service import process_json_query
 from .file_handlers.dxf_console_service import convert_dxf_upload_to_json
 from .file_handlers.gsfx_upload_service import convert_gsfx_upload_to_json
@@ -9,7 +8,6 @@ from .vision import process_vision_query
 
 __all__ = [
     "run_console_json_ollama",
-    "run_console_vision_ollama",
     "convert_dxf_upload_to_json",
     "convert_gsfx_upload_to_json",
     "convert_arp_upload_to_json",
